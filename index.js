f43c03e435c5dfc45c06f9269a2c5ca06a2f6a60
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-03 03:11:48 -0800

    Adds myfile

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [ "Milo", "Otis", "Garfield" ];
+function destructivelyAppendCat(name){
+    return cats.push(name); 
+}
+destructivelyAppendCat(Ralph);
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+destructivelyPrependCat(Bob);
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name){
+    return [...cats, name];
+} 
+appendCat("Broom");
+
+function prependCat(name){
+    return [name, ...cats];
+}
+prependCat(Arnold);
+
+function removeLastCat(){
+    return cats.slice(0,2);
+}
+removeLastCat()
+
+function removeFirstCat(){
+    return cats.slice(1,4);
+}
+removeFirstCat();
+
